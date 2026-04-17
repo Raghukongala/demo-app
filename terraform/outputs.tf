925512f0661d75b16b0ee19d4baa0196ffdf70aa
Raghu <raghukongala406@gmail.com>
2026-04-17 22:56:07 +0530

    Add Jenkinsfile for app deployment pipeline

---
+++ terraform/outputs.tf
@@ -0,0 +1,19 @@
+output "cluster_name" {
+  value = aws_eks_cluster.main.name
+}
+
+output "cluster_endpoint" {
+  value = aws_eks_cluster.main.endpoint
+}
+
+output "cluster_ca" {
+  value = aws_eks_cluster.main.certificate_authority[0].data
+}
+
+output "ecr_urls" {
+  value = { for k, v in aws_ecr_repository.repos : k => v.repository_url }
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
